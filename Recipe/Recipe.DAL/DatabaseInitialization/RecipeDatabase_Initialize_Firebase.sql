101e85d7dd33113faff56b557135f425fd7d3efb
Valentin Tkalec <valentintkalec@hotmail.com>
2022-04-12 21:49:06 +0200

    Added RecipeContext class

---
+++ Recipe/Recipe.DAL/DatabaseInitialization/RecipeDatabase_Initialize_Firebase.sql
@@ -0,0 +1,149 @@
+--CREATE DATABASE RecipeDatabaseFirebase;
+--Use Database 'RecipeDatabaseFirebase'
+USE RecipeDatabaseFirebase;
+
+--Create table 'User'
+CREATE TABLE [User]
+	(
+		UserID               INT            NOT NULL,
+		FirebaseUserID       NVARCHAR (450) NOT NULL,
+		UserName             NVARCHAR (256) NOT NULL,
+		Email                NVARCHAR (256) NOT NULL,
+		FirstName            NVARCHAR (256) NOT NULL,
+		LastName             NVARCHAR (256) NOT NULL,
+
+		-- PRIMARY + UNIQUE
+		CONSTRAINT PK_User PRIMARY KEY CLUSTERED (UserID ASC),
+	);
+
+		-- INDEX for table 'User'
+		CREATE UNIQUE NONCLUSTERED INDEX IX_User_Email_FirebaseUserID
+		ON [User] (Email, FirebaseUserID ASC);
+
+	--Create table 'Recipe'
+	CREATE TABLE Recipe
+	(
+		RecipeID      INT IDENTITY(1,1) NOT NULL,
+		[Name]        NVARCHAR(255)     NOT NULL,
+		[Description] NVARCHAR(255)     NOT NULL,
+		UserID        INT               NOT NULL
+			
+		-- PRIMARY + UNIQUE
+		CONSTRAINT PK_Recipe PRIMARY KEY (RecipeID),
+
+		--FK
+		CONSTRAINT FK_Recipe_User FOREIGN KEY (UserID) REFERENCES [User] (UserID)
+	);
+
+	--Create table 'RecipeAttributes'
+	CREATE TABLE RecipeAttributes
+	(
+		RecipeAttributesID INT IDENTITY(1,1) NOT NULL,
+		RecipeID      INT                    NOT NULL,
+		CreatedDate   DATETIME               NOT NULL CONSTRAINT DF_RecipeAttributes_CreatedDate DEFAULT CURRENT_TIMESTAMP,
+		Person        INT                    NOT NULL,
+		PrepareTime   TIME                   NOT NULL,
+		Serving       NVARCHAR(255)	         NOT NULL,
+		Advice        NVARCHAR(255)	         NULL,
+		Stars         INT			         NOT NULL CONSTRAINT DF_RecipeAttributes_Stars DEFAULT 0,
+		Likes         INT			         NOT NULL CONSTRAINT DF_RecipeAttributes_Likes DEFAULT 0
+
+		--UC
+		CONSTRAINT UC_RecipeAttributes_RecipeID UNIQUE (RecipeID),
+
+		--CK
+		CONSTRAINT CK_Recipe_Person CHECK (Person >= 1),
+		CONSTRAINT CK_Recipe_Stars CHECK (Stars >= 0),
+		CONSTRAINT CK_Recipe_Likes CHECK (Likes >= 0),
+		CONSTRAINT CK_Recipe_CreatedDate CHECK (CreatedDate <= GETDATE()),
+
+		--FK
+		CONSTRAINT FK_RecipeAttributes_Recipe FOREIGN KEY (RecipeID) REFERENCES dbo.Recipe (RecipeID)
+	);
+
+	--Create table 'PreparationStep'
+	CREATE TABLE PreparationStep
+	(
+		PreparationStepID INT IDENTITY(1,1) NOT NULL,
+		RecipeID          INT               NOT NULL,
+		Number            INT               NOT NULL,
+		[Description]     TEXT              NOT NULL
+
+		-- PRIMARY + UNIQUE
+		CONSTRAINT PK_Step PRIMARY KEY (PreparationStepID),
+
+		--CK
+		CONSTRAINT CK_Step_Number CHECK (Number >= 1),
+
+		--FK
+		CONSTRAINT FK_Step_Recipe FOREIGN KEY (PreparationStepID) REFERENCES dbo.Recipe (RecipeID)
+	);
+
+	--Create table 'Picture'
+	CREATE TABLE [Picture]
+	(
+		PictureID INT IDENTITY(1,1) NOT NULL,
+		RecipeID  INT               NOT NULL,
+		[Image] IMAGE             NOT NULL
+
+		-- PRIMARY + UNIQUE
+		CONSTRAINT PK_Picture PRIMARY KEY (PictureID),
+
+		--FK
+		CONSTRAINT FK_Picture_Recipe FOREIGN KEY (RecipeID) REFERENCES Recipe (RecipeID)
+	);
+
+	--Create table 'MeasuringUnit'
+	CREATE TABLE MeasuringUnit
+	(
+		MeasuringUnitID INT IDENTITY(1,1) NOT NULL,
+		[Name]          NVARCHAR(50)      NOT NULL,
+		Abbreviation    NVARCHAR(10)      NOT NULL
+
+		-- PRIMARY + UNIQUE
+		CONSTRAINT PK_MeasuringUnit PRIMARY KEY (MeasuringUnitID),
+		CONSTRAINT UC_Ingredient_Name_Abbreviation UNIQUE ([Name], Abbreviation)
+	);
+
+	--Create table 'Ingredient'
+	CREATE TABLE Ingredient
+	(
+		IngredientID INT IDENTITY(1,1) NOT NULL,
+		[Name]       NVARCHAR(255)     NOT NULL
+
+		-- PRIMARY + UNIQUE
+		CONSTRAINT PK_Ingredient PRIMARY KEY (IngredientID),
+		CONSTRAINT UC_Ingredient_Name UNIQUE ([Name]),
+	);
+
+	--Create table 'IngredientMeasuringUnit'
+	CREATE TABLE IngredientMeasuringUnit
+	(
+		IngredientID     INT NOT NULL,
+		MeasuringUnitID  INT NOT NULL
+
+		-- PRIMARY + UNIQUE
+		CONSTRAINT PK_IngredientMeasuringUnit_IngredientID_MeasuringUnitID PRIMARY KEY (IngredientID, MeasuringUnitID),
+
+		--FK
+		CONSTRAINT FK_IngredientMeasuringUnit_IngredientID FOREIGN KEY (IngredientID) REFERENCES dbo.Ingredient (IngredientID)
+		ON DELETE CASCADE,
+		CONSTRAINT FK_IngredientMeasuringUnit_MeasuringUnitID FOREIGN KEY (MeasuringUnitID) REFERENCES dbo.MeasuringUnit (MeasuringUnitID)
+		ON DELETE CASCADE
+	);
+
+	--Create table 'UserLikedRecipe'
+	CREATE TABLE UserLikedRecipe
+	(
+		UserID   INT NOT NULL,
+		RecipeID INT NOT NULL
+
+		-- PRIMARY + UNIQUE
+		CONSTRAINT UC_UserLikedRecipe_UserID_RecipeID PRIMARY KEY (UserID, RecipeID),
+
+		--FK
+		CONSTRAINT FK_UserLikedRecipe_UserID FOREIGN KEY (UserID) REFERENCES dbo.[User] (UserID)
+		ON DELETE CASCADE,
+		CONSTRAINT FK_UserLikedRecipe_RecipeID FOREIGN KEY (RecipeID) REFERENCES dbo.Recipe (RecipeID)
+		ON DELETE CASCADE
+	);
---
+++ Recipe/Recipe.DAL/RecipeContext.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe.DAL
+{
+    public class RecipeContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public RecipeContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+
+        public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters)
+        {
+            using IDbConnection dbConnection = CreateConnection();
+
+            return await dbConnection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(string storedProcedure, T parameters)
+        {
+            using IDbConnection dbConnection = CreateConnection();
+
+            await dbConnection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
