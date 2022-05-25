4d3a0a9fec149f6a912af0bb49f746d97d386c90
Valentin Tkalec <valentintkalec@hotmail.com>
2022-05-25 22:51:16 +0200

    Added Automapper extension method summaries

--- Recipe/Recipe.Helpers/AutoMapper/AutoMapperExtension.cs
+++ Recipe/Recipe.Helpers/AutoMapper/AutoMapperExtension.cs
@@ -8,11 +8,24 @@ namespace Recipe.Helpers.AutoMapper
 {
     public static class AutoMapperExtension
     {
+        /// <summary>
+        /// Method merges two objects into one
+        /// </summary>
+        /// <typeparam name="TResult">Destination object</typeparam>
+        /// <param name="item1">First object you want to merge</param>
+        /// <param name="item2">Second object you want to merge</param>
+        /// <returns>TResult</returns>
         public static TResult MergeInto<TResult>(this IMapper mapper, object item1, object item2)
         {
             return mapper.Map(item2, mapper.Map<TResult>(item1));
         }
 
+        /// <summary>
+        /// Method merges multiple objects into one
+        /// </summary>
+        /// <typeparam name="TResult">Destination object</typeparam>
+        /// <param name="objects">objects you want to merge</param>
+        /// <returns>TResult</returns>
         public static TResult MergeInto<TResult>(this IMapper mapper, params object[] objects)
         {
             var res = mapper.Map<TResult>(objects.First());
