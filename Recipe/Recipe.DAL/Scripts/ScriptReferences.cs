514f255fdec6431bf013e38495ed3e90c2ccbfbe
Valentin Tkalec <valentintkalec@hotmail.com>
2022-06-01 13:50:08 +0200

    Added delete and update stored procedures for other models

--- Recipe/Recipe.DAL/Scripts/ScriptReferences.cs
+++ Recipe/Recipe.DAL/Scripts/ScriptReferences.cs
@@ -18,18 +18,24 @@ namespace Recipe.DAL.Scripts
         {
             public static string SP_CreateUserData = nameof(SP_CreateUserData);
             public static string SP_RetrieveUserData = nameof(SP_RetrieveUserData);
+            public static string SP_DeleteUserData = nameof(SP_DeleteUserData);
+            public static string SP_UpdateUserData = nameof(SP_UpdateUserData);
         }
 
         public static class Category
         {
             public static string SP_CreateCategory = nameof(SP_CreateCategory);
             public static string SP_RetrieveCategory = nameof(SP_RetrieveCategory);
+            public static string SP_DeleteCategory = nameof(SP_DeleteCategory);
+            public static string SP_UpdateCategory = nameof(SP_UpdateCategory);
         }
 
         public static class Subcategory
         {
             public static string SP_CreateSubcategory = nameof(SP_CreateSubcategory);
             public static string SP_RetrieveSubcategory = nameof(SP_RetrieveSubcategory);
+            public static string SP_DeleteSubcategory = nameof(SP_DeleteSubcategory);
+            public static string SP_UpdateSubcategory = nameof(SP_UpdateSubcategory);
         }
     }
 }
